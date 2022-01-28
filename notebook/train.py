9586c0f4c3e434c0d142844a3bb9c607498eed83
skywalker0803r <skywalker0803r@gmail.com>
2022-01-28 15:18:10 +0800

    加入新算法

--- notebook/train.py
+++ notebook/train.py
@@ -5,48 +5,29 @@ import numpy as np
 import joblib
 
 # 導入演算法
-url = 'https://gist.githubusercontent.com/skywalker0803r/7d1c5c3e8f11b6713a7994f08b62fbe9/raw/51992fcd154ea85b99f9332165fb5b4275139138/knn_nlp.py'
+url = 'https://gist.githubusercontent.com/skywalker0803r/14922b3cb7ca23e0de5eec59a69cec25/raw/fc99cfce652af5214867936363fc254fe68c7dc2/CountVectorizer_NearestNeighbors.py'
 exec(requests.get(url).text)
 
-
-# 預處理函數
-def preprocess(x):
-    x = str(x).upper() # 轉大寫字串
-    x = re.sub('[\u4e00-\u9fa5]', '', x) # 去除中文
-    x = re.sub(r'[^\w\s]','',x) # 去除標點符號
-    x = x.replace('\n','').replace('\r','').replace('\t','') # 換行符號去除
-    return str(x)
-
 # 輸入欄位
-x_col = ['45A','50','59','46A','47A','78']
-# database
-EXPNO對應表 = pd.read_csv('../data/對應表/EXPNO對應表.csv')#.head()
-# 對輸入字串預處理
-for col in x_col:
-    EXPNO對應表[col] = EXPNO對應表[col].apply(preprocess)
-str_max_len = 300
+x_col = ['45A']#,'50','59','46A','47A','78']
+
+# EXPNO對應表
+EXPNO對應表 = pd.read_csv('../data/對應表/EXPNO對應表.csv').head(50)
 EXPNO對應表['X'] = EXPNO對應表[x_col].sum(axis=1) # 字串串起來
-EXPNO對應表['X'] = EXPNO對應表['X'].apply(lambda x:str(x)[:str_max_len]) # 截斷
 
 # 訓練資料
 train_data = EXPNO對應表['X'].values.tolist()
 
 # 開始訓練模型
-model = coresystem(train_data,str_max_len=str_max_len)
-
-# save index
-model.index.saveIndex('index')
-
-# load index
-model.index.loadIndex('index')
+model = coresystem()
+model.fit(train_data)
 
 # test
-test_data = np.random.choice(train_data,size=3).tolist()
+test_data = train_data[:50]
 最相近鄰居 = model.predict(test_data)
 for i,j in zip(test_data,最相近鄰居):
   assert i==j
 
-#
 print('all ok')
 
 
