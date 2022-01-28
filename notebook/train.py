8984dd6831416d88a3d30bf1dde0ec7bdc9df7c9
skywalker0803r <skywalker0803r@gmail.com>
2022-01-28 15:56:27 +0800

    還在測試

--- notebook/train.py
+++ notebook/train.py
@@ -12,7 +12,7 @@ exec(requests.get(url).text)
 x_col = ['45A','50','59','46A','47A','78']
 
 # EXPNO對應表
-EXPNO對應表 = pd.read_csv('../data/對應表/EXPNO對應表.csv')
+EXPNO對應表 = pd.read_csv('../data/對應表/EXPNO對應表.csv').astype(str)
 EXPNO對應表['X'] = EXPNO對應表[x_col].sum(axis=1) # 字串串起來
 
 # 訓練資料
@@ -22,15 +22,8 @@ train_data = EXPNO對應表['X'].values.tolist()
 model = coresystem()
 model.fit(train_data)
 
-# test
-test_data = train_data
-最相近鄰居 = model.predict(test_data)
-for i,j in zip(test_data,最相近鄰居):
-  assert i==j
-print('test ok')
-
 # save model
-save_path = '../models/model'
+save_path = '../models/model.pkl'
 joblib.dump(model,save_path)
 print(f'save done save model in {save_path}')
 
