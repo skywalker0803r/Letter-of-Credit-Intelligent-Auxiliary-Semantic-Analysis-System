4aa4088b2a6e85efdeb226626a50a8eea74ba3b9
skywalker0803r <skywalker0803r@gmail.com>
2022-02-07 08:38:34 +0800

    update

--- notebook/train.py
+++ notebook/train.py
@@ -4,26 +4,34 @@ import re
 import numpy as np
 import joblib
 
-# 導入演算法
-url = 'https://gist.githubusercontent.com/skywalker0803r/14922b3cb7ca23e0de5eec59a69cec25/raw/fc99cfce652af5214867936363fc254fe68c7dc2/CountVectorizer_NearestNeighbors.py'
+# algo
+url = 'https://gist.githubusercontent.com/skywalker0803r/0b78f6f77a6ec72195cb6a7feb3f5a7c/raw/78b41b87f722953d080f91b5527cfa58f65bc518/Text%2520similarity%2520comparison%2520with%2520Bert%2520and%2520nmslib%2520and%2520clean%2520function.py'
 exec(requests.get(url).text)
 
-# 輸入欄位
+# input
 x_col = ['45A','50','59','46A','47A','78']
 
-# EXPNO對應表
-EXPNO對應表 = pd.read_csv('../data/對應表/EXPNO對應表.csv').astype(str)
-EXPNO對應表['X'] = EXPNO對應表[x_col].sum(axis=1) # 字串串起來
+# load combined_excel
+df = pd.read_excel('../data/combined_excel.xlsx').astype(str)
+df['x'] = df[x_col].sum(axis=1) # concat
+df['x'] = df['x'].apply(clean_text) # preprocess
 
-# 訓練資料
-train_data = EXPNO對應表['X'].values.tolist()
+# save preprocessed data
+df.to_csv('../data/combined_excel_addx.csv')
 
-# 開始訓練模型
-model = coresystem()
-model.fit(train_data)
+# training model
+model = coresystem(data=df['x'].values.tolist()[:5],str_max_len=500)
 
-# save model
-save_path = '../models/model.pkl'
-joblib.dump(model,save_path)
-print(f'save done save model in {save_path}')
+# save index
+model.index.saveIndex('../models/model_index')
+
+# load index
+model.index.loadIndex('../models/model_index')
+
+# test model
+x_test = df['x'].values.tolist()[:1]
+y_pred = model.predict(x_test)
+assert x_test == y_pred
+
+print('all done!')
 
